694e37e3391450fb4f8af223a5df73d369c11f95
VidDev2 <Dev2@gmail.com>
2017-05-01 01:00:16 +0530

    Dev2 Kept changed project

--- TestProj1/src/com/nt/git/Testing.java
+++ TestProj1/src/com/nt/git/Testing.java
@@ -5,6 +5,7 @@ public class Testing {
 	public static void main(String[] args) {
 		 System.out.println("Wlcome to Git");
 		 System.out.println("Wlcome to Git---->Dev1");
+		 System.out.println("Wlcome to Git---->Dev2");
 
 	}
 
